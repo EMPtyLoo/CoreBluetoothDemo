00720904db5cff65b3530d9d161c067f8fe6b98b
EMPtyLoo <lukeok2008@gmail.com>
2016-03-31 00:22:21 +0800

    Initial Commit

---
+++ CoreBluetoothDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreBluetoothDemo
+//
+//  Created by EMPty on 3/31/16.
+//  Copyright (c) 2016 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreBluetoothDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreBluetoothDemo
+//
+//  Created by EMPty on 3/31/16.
+//  Copyright (c) 2016 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
